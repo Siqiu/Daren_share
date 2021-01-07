8194ffadcfa8350ca153ddd05fcc02bdc5d93b3c
Zhengxiong Wang <donatello.wang@outlook.com>
2021-01-07 11:06:18 +0800

    Main##王政雄##分离AT成单独的模块。

---
+++ protocol/IoT/protocal_at_checkbyte/protocal_at_checkbyte.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Daren Hi-Tech Electronics Co., Ltd. Development Team 2020-2020. All rights reserved.
+ * File name:   protocal_at_checkbyte.h
+ * Author: Donn1e   ID:     Version:    Date: 07/01/2021
+ * Description: AT指令的异或校验
+ * Others:
+ * History:
+ */
+
+#ifndef DAREN_SHARE_PROTOCAL_AT_CHECKBYTE_H__
+#define DAREN_SHARE_PROTOCAL_AT_CHECKBYTE_H__
+
+#include <libc.h>
+
+uint8_t at_protocol_check_byte(uint8_t *buf, uint16_t len);
+uint8_t get_check_byte(uint8_t *buf);
+
+#endif //DAREN_SHARE_PROTOCAL_AT_CHECKBYTE_H__
